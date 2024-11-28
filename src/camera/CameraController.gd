0e4018fb3246d29a54476e1ec075e018bf03fd41
JoeW <industrialdonut@gmail.com>
2024-11-27 22:51:47 -0800

    feat: add capture/release mouse, upper/lower limit degrees to CameraController

--- src/camera/CameraController.gd
+++ src/camera/CameraController.gd
@@ -1,25 +1,30 @@
 extends Node3D
 
-@export var default_zoom = 4.0 # default units back from center point
 @export var mouse_sensitivity = 0.01
-
-func _ready() -> void:
-	$VBase/Camera3D.position.z = default_zoom
-
+@export var upper_limit_degrees = 90
+@export var lower_limit_degrees = -90
 
 func _input(event: InputEvent) -> void:
-	#if Input.is_action_pressed("rmb"):
-		#return
+	if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
+		return
 	
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$VBase.rotate_x(-event.relative.y * mouse_sensitivity)
 	
-	$VBase.rotation_degrees.x = clamp($VBase.rotation_degrees.x, -90, 90)
+	$VBase.rotation_degrees.x = clamp($VBase.rotation_degrees.x, lower_limit_degrees, upper_limit_degrees)
 
 
 func get_camera_global_transform() -> Transform3D:
 	return $VBase/Camera3D.global_transform
+
+
+func capture_mouse() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func release_mouse() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 
 func set_fov(deg : float) -> void:
