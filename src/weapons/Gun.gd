e0548b38f7dfe708ec7fb6828a7755a1b9897b8a
JoeW <industrialdonut@gmail.com>
2024-12-02 21:10:28 -0800

    refactor: shoot_pressed and shoot_released signatures now used on Gun

--- src/weapons/Gun.gd
+++ src/weapons/Gun.gd
@@ -1,7 +1,11 @@
 extends Node3D
 
-func shoot():
+func shoot_pressed():
 	$RayCast3D.force_raycast_update()
 	var t = $RayCast3D.get_collider()
 	if $RayCast3D.is_colliding():
 		t.hit()
+
+
+func shoot_released():
+	pass
